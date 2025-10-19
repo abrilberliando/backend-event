920f3c20bd0818cf3aedbf04e0398722132b3ad2
abrilberliando <abrilbrlnd2@gmail.com>
2025-10-20 06:37:41 +0700

    feat: add swagger

---
+++ src/docs/swagger.ts
@@ -0,0 +1,39 @@
+import swaggerAutogen from "swagger-autogen";
+
+const doc = {
+  info: {
+    version: "v1.0.0",
+    title: "Swagger Demo Project",
+    description: "Implementation of Swagger with TypeScript",
+  },
+
+  servers: [
+    {
+      url: "http://localhost:3000/api",
+      description: "Local Development",
+    },
+    {
+      url: "https://back-end-acara.vercel.app/api",
+      description: "",
+    },
+  ],
+  components: {
+    securitySchemes: {
+      bearerAuth: {
+        type: "http",
+        scheme: "bearer",
+      },
+    },
+    schemas: {
+      LoginRequest: {
+        identifier: "lalajola765@gmail.com",
+        password: "Y12345678",
+      },
+    },
+  },
+};
+
+const outputFile = "./swagger_output.json";
+const endpointsFiles = ["../routes/api.ts"];
+
+swaggerAutogen({ openapi: "3.0.0" })(outputFile, endpointsFiles, doc);
